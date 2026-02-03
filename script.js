7a2b9182e7628cb64a538174a04bc6ea2e85e624
Sofie01767 <sophiaenoch998@gmail.com>
2026-02-03 21:21:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+// Button interaction
+const joinBtn = document.getElementById("joinBtn");
+
+joinBtn.addEventListener("click", function () {
+    alert("Welcome to NexaDev Academy! 🚀\nYour tech journey starts now.");
+});
